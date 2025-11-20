c4302792ebd03608713f6a386c4afd439e4f7721
SvetlanaShkryleva <114089663+SvetlanaShkryleva@users.noreply.github.com>
2025-11-20 20:00:36 +0300

    clang-tidy

--- tasks/shkryleva_s_vec_min_val/common/include/common.hpp
+++ tasks/shkryleva_s_vec_min_val/common/include/common.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <algorithm>
 #include <string>
 #include <tuple>
 #include <vector>
--- tasks/shkryleva_s_vec_min_val/mpi/src/ops_mpi.cpp
+++ tasks/shkryleva_s_vec_min_val/mpi/src/ops_mpi.cpp
@@ -2,6 +2,7 @@
 
 #include <mpi.h>
 
+#include <algorithm>
 #include <climits>
 #include <vector>
 
