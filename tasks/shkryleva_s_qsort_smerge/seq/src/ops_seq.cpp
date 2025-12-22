886cd808b85d2830d4d40197fc39d67615faa8fd
SvetlanaShkryleva <114089663+SvetlanaShkryleva@users.noreply.github.com>
2025-12-22 09:50:35 +0300

    seq6

--- tasks/shkryleva_s_qsort_smerge/seq/src/ops_seq.cpp
+++ tasks/shkryleva_s_qsort_smerge/seq/src/ops_seq.cpp
@@ -1,6 +1,7 @@
 #include "shkryleva_s_qsort_smerge/seq/include/ops_seq.hpp"
 
 #include <algorithm>
+#include <functional>
 #include <vector>
 
 #include "shkryleva_s_qsort_smerge/common/include/common.hpp"
@@ -22,33 +23,60 @@ bool ShkrylevaSQSortSMergeSEQ::PreProcessingImpl() {
 }
 
 std::vector<int> ShkrylevaSQSortSMergeSEQ::QuickSortWithMerge(const std::vector<int> &arr) {
-  if (arr.size() <= 1) {
-    return arr;
+  if (arr.empty()) {
+    return {};
   }
 
-  std::vector<int> result = arr;
+  if (arr.size() == 1) {
+    return {arr[0]};
+  }
 
-  std::function<void(int, int)> quick_sort = [&](int low, int high) {
-    if (low < high) {
-      int pivot = result[(low + high) / 2];
-      int i = low, j = high;
+  std::vector<int> result;
+  result.reserve(arr.size());
 
-      while (i <= j) {
-        while (result[i] < pivot) {
-          i++;
-        }
-        while (result[j] > pivot) {
-          j--;
-        }
-        if (i <= j) {
-          std::swap(result[i], result[j]);
-          i++;
-          j--;
-        }
+  for (int value : arr) {
+    result.push_back(value);
+  }
+
+  std::function<void(int, int)> quick_sort = [&result](int left, int right) {
+    if (left >= right) {
+      return;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    if (result[right] < result[left]) {
+      std::swap(result[left], result[right]);
+    }
+    if (result[mid] < result[left]) {
+      std::swap(result[mid], result[left]);
+    }
+    if (result[right] < result[mid]) {
+      std::swap(result[right], result[mid]);
+    }
+
+    int pivot = result[mid];
+    int i = left, j = right;
+
+    while (i <= j) {
+      while (result[i] < pivot) {
+        i++;
       }
+      while (result[j] > pivot) {
+        j--;
+      }
+      if (i <= j) {
+        std::swap(result[i], result[j]);
+        i++;
+        j--;
+      }
+    }
 
-      quick_sort(low, j);
-      quick_sort(i, high);
+    if (left < j) {
+      quick_sort(left, j);
+    }
+    if (i < right) {
+      quick_sort(i, right);
     }
   };
 
