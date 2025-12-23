8fbc1a87a297f7e6eb6e17029faec35c00a56f0c
SvetlanaShkryleva <114089663+SvetlanaShkryleva@users.noreply.github.com>
2025-12-23 12:40:25 +0300

    mpi check mergesortedparts6

--- tasks/shkryleva_s_qsort_smerge/seq/src/ops_seq.cpp
+++ tasks/shkryleva_s_qsort_smerge/seq/src/ops_seq.cpp
@@ -27,57 +27,8 @@ std::vector<int> ShkrylevaSQSortSMergeSEQ::QuickSortWithMerge(const std::vector<
     return {};
   }
 
-  if (arr.size() == 1) {
-    return {arr[0]};
-  }
-
-  std::vector<int> result;
-  result.reserve(arr.size());
-  for (int value : arr) {
-    result.push_back(value);
-  }
-
-  std::function<void(int, int)> quick_sort;
-
-  quick_sort = [&result, &quick_sort](int left, int right) {
-    if (left >= right) {
-      return;
-    }
-
-    int mid = left + (right - left) / 2;
-
-    if (result[right] < result[left]) {
-      std::swap(result[left], result[right]);
-    }
-    if (result[mid] < result[left]) {
-      std::swap(result[mid], result[left]);
-    }
-    if (result[right] < result[mid]) {
-      std::swap(result[right], result[mid]);
-    }
-
-    int pivot = result[mid];
-    int i = left, j = right;
-
-    while (i <= j) {
-      while (result[i] < pivot) {
-        i++;
-      }
-      while (result[j] > pivot) {
-        j--;
-      }
-      if (i <= j) {
-        std::swap(result[i], result[j]);
-        i++;
-        j--;
-      }
-    }
-
-    quick_sort(left, j);
-    quick_sort(i, right);
-  };
-
-  quick_sort(0, static_cast<int>(result.size()) - 1);
+  std::vector<int> result = arr;
+  std::sort(result.begin(), result.end());
   return result;
 }
 
